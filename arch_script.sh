0ec7115fd291ffdc55017a443f1e25a6b9c19011
sadako <deividkiller9@gmail.com>
2026-03-06 14:26:12 +0100

    now better but need tests

--- arch_script.sh
+++ arch_script.sh
@@ -9,8 +9,8 @@ echo "==== ARCH INSTALL SCRIPT ===="
 umount -R /mnt 2>/dev/null || true
 
 echo "Available disks:"
-lsblk -o NAME,SIZE,TYPE,FSTYPE
-read -p "Disk to install to (ex: nvme0n1): " DISK
+lsblk -o NAME,SIZE,FSUSE%,TYPE,FSTYPE,MOUNTPOINTS,UUID
+read -p "Disk to install Arch to -! DATA WILL BE DELETED !- (ex: nvme0n1): " DISK
 EFI=${DISK}p1
 ROOT=${DISK}p2
 if [[ $DISK == sd* ]]; then
@@ -37,12 +37,12 @@ btrfs subvolume create /mnt/@pkg
 btrfs subvolume create /mnt/@snapshots
 umount /mnt
 echo "Mounting subvolumes..."
-mount -o compress=zstd,subvol=@ /dev/$ROOT /mnt
+mount -o compress=zstd:1,noatime,subvol=@ /dev/$ROOT /mnt
 mkdir -p /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
-mount -o compress=zstd,subvol=@home /dev/$ROOT /mnt/home
-mount -o compress=zstd,subvol=@log /dev/$ROOT /mnt/var/log
-mount -o compress=zstd,subvol=@pkg /dev/$ROOT /mnt/var/cache/pacman/pkg
-mount -o compress=zstd,subvol=@snapshots /dev/$ROOT /mnt/.snapshots
+mount -o subvol=@home /dev/$ROOT /mnt/home
+mount -o subvol=@log /dev/$ROOT /mnt/var/log
+mount -o subvol=@pkg /dev/$ROOT /mnt/var/cache/pacman/pkg
+mount -o subvol=@snapshots /dev/$ROOT /mnt/.snapshots
 mount /dev/$EFI /mnt/boot
 
 echo "Optimizing mirrors..."
@@ -62,7 +62,6 @@ pacstrap /mnt \
  nano \
  git \
  networkmanager \
- snapper \
  htop \
  fastfetch \
  bluez \
@@ -71,6 +70,8 @@ pacstrap /mnt \
  efibootmgr \
  mtools \
  dosfstools
+#  snapper \
+#  grub-btrfs
 
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -106,6 +107,7 @@ sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 echo "Enabling basic services"
 systemctl enable NetworkManager
 systemctl enable bluetooth
+systemctl enable fstrim.timer
 
 echo "Installing CPU microcode"
 if grep -q "Intel" /proc/cpuinfo; then
@@ -113,26 +115,52 @@ if grep -q "Intel" /proc/cpuinfo; then
 elif grep -q "AMD" /proc/cpuinfo; then
  pacman -S --noconfirm amd-ucode
 fi
-
 echo "Checking for NVIDIA GPU"
 if lspci | grep -i nvidia; then
  pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 fi
+mkinitcpio -P
 
 echo "Installing GRUB bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+lsblk -o NAME,SIZE,FSUSE%,TYPE,FSTYPE,MOUNTPOINTS,UUID
+read -p "Add second entry point to GRUB? (y/n): " ENABLE_DBOOT
+if [[ "${ENABLE_DBOOT,,}" == "y" || "${ENABLE_DBOOT,,}" == "yes" ]]; then
+ read -p "Second EFI partition ((ex: nvme1n1p1)): " PART_DBOOT
+ sudo mount --mkdir /dev/$PART_DBOOT /mnt/efi2
+ pacman -S --noconfirm os-prober ntfs-3g
+ sed -i 's/^#GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+ grep -q GRUB_DISABLE_OS_PROBER /etc/default/grub || echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+fi
 grub-mkconfig -o /boot/grub/grub.cfg
 
 EOF
+
+# zram
+arch-chroot /mnt bash -c '
+cat > /etc/systemd/zram-generator.conf <<EOF
+[zram0]
+zram-size = min(ram / 2, 8192)
+compression-algorithm = zstd
+swap-priority = 100
+EOF
+cat > /etc/sysctl.d/99-zram.conf <<EOF
+vm.swappiness=180
+vm.page-cluster=0
+EOF
+'
+arch-chroot /mnt systemctl enable systemd-zram-setup@zram0.service
 
 #echo "Setup snapper"
 #snapper --no-dbus -c root create-config /
 #systemctl enable snapper-timeline.timer
 #systemctl enable snapper-cleanup.timer
 
+# TODO: xcfe as 2nd
 read -p "Install a Desktop Environment? (KDE Plasma will be used) (y/n)" ENABLE_KDE
 if [[ "${ENABLE_KDE,,}" == "y" || "${ENABLE_KDE,,}" == "yes" ]]; then
- pacstrap /mnt plasma sddm konsole dolphin --noconfirm
+ pacstrap /mnt plasma wayland sddm konsole dolphin kscreen kwrite breeze-gtk --noconfirm
  arch-chroot /mnt systemctl enable sddm
 fi
 
@@ -148,6 +176,12 @@ if [[ "${ENABLE_CURL,,}" == "y" || "${ENABLE_CURL,,}" == "yes" ]]; then
   -o /mnt/home/\$(ls /mnt/home)/install_optionals.sh
   chmod +x /mnt/home/\$(ls /mnt/home)/install_optionals.sh
 fi
+
+# TODO ufw
+
+# TODO lib32
+
+# TODO yay
 
 echo "Finished and rebooting in 5 seconds..."
 umount -lR /mnt
